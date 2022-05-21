44bebfa9f87b921d5bd4086f93b3f29143cdd2a1
Sasikant Biswal <biswalsashikant@gmail.com>
2022-05-22 01:48:11 +0530

    Added some head scratching chanllenging code

--- eventDelegation/index.js
+++ eventDelegation/index.js
@@ -6,3 +6,8 @@ document.querySelector('#products').addEventListener('click', (event) => {
 
 // event delegation describes that without adding event listeners to each specific list items 
 // we just added it to the parent and we are using to click other list items.
+
+
+
+// ******** Follow Javascript notes ********** OR
+// ***********  Javascripttutorial.net event delegation topics in event chapter ************
--- outputQuestion/stringConcatatenation.js
+++ outputQuestion/stringConcatatenation.js
@@ -8,3 +8,18 @@ console.log(def); // 726
 
 var ghi = 4 + 3 + 2 + "6";
 console.log(ghi);   // 96
+
+
+console.log("=================================================================================")
+
+console.log(4 - "2");       // => 2
+console.log("2" - 4);       // => -2
+console.log("2" + 4);       // => 24
+console.log(7 + "2" - 2);   // => 70
+console.log("5" - "2");     // => 3
+console.log("5" - "2" + 3);     // => 6
+console.log("5" - "2" + "1");   // => 31
+console.log("7" - "2" + "-1");  // => 5 -1
+console.log("7" + "2" + "-1");  // => 72 -1
+console.log("7" + "2" - "-1");  // => 73
+console.log("7" + "2" - "1");  // => 71
