5d89f13d3f4ea02728cb77924a8f9d90eb280f11
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-10-18 10:17:11 +0530

    Added some copying challenges

--- outputQuestion/copyingObjects.js
+++ outputQuestion/copyingObjects.js
@@ -18,3 +18,27 @@ director.salary = 100000;
 
 console.log(employee);  // { name: 'Arjun', age: 28, salary: 60000 }
 console.log(director);  // { name: 'Arjun', age: 28, salary: 100000 }
+
+
+// NESTED OBJECTS
+
+//use deep copy
+let oldEmployee = {
+    id: '102',
+  
+    name: 'Jack',
+  
+    address: {
+      city: 'New York',
+  
+      country: 'USA',
+    },
+  };
+  
+  let newEmployee = JSON.parse(JSON.stringify(oldEmployee));
+  
+  newEmployee.address.city = 'Texas';
+  
+  console.log(oldEmployee.address.city);    // New York
+  
+  console.log(newEmployee.address.city);    // Texas
